451ab61d004cd18fdcfa61d22362d558f767862a
KyleHCode <kyleholmberg9@gmail.com>
2025-10-28 15:39:20 -0400

    Began working on photoresistor project to get more familiar with analog inputs. Was not able to complete today due to esp32 chip being too big for one breadboard. Will continue tomorrow once I have a second breadboard to connect esp32 over both boards for full access to all pins.

--- src/blinkphotoresistor.cpp
+++ src/blinkphotoresistor.cpp
@@ -1,3 +1,17 @@
 #include <Arduino.h>
 #define LED_BUILTIN 2
-#define PHOTORESISTOR_PIN 
+#define PHOTORESISTOR_INPUT_PIN 35
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+    int delay_val = analogRead(PHOTORESISTOR_INPUT_PIN)
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(delay_val);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(delay_val);
+}
